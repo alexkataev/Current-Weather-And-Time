ca099b2e72de138efa032812576a1aa06e60f721
alexkataev <39487162+alexkataev@users.noreply.github.com>
2020-11-11 13:22:10 +0300

    Content initial state added

---
+++ src/app/content/init-state/init-state.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'init-state',
+  templateUrl: './init-state.component.html',
+  styleUrls: ['../content.component.css', './init-state.component.css']
+})
+
+export class InitStateComponent {
+
+  constructor(
+  ) {
+  }
+
+}
